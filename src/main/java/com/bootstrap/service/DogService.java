25cc3c7a1249e781ee5572e568dff0ef5b7daa5a
Hanwo <hhuang51@hawk.iit.edu>
2021-02-08 13:53:50 -0600

    Initial commit

---
+++ src/main/java/com/bootstrap/service/DogService.java
@@ -0,0 +1,13 @@
+package com.bootstrap.service;
+
+import com.bootstrap.entity.Dog;
+
+import java.util.List;
+
+public interface DogService {
+
+    List<Dog> retrieveDogs();
+    List<String> retrieveDogBreed();
+    String retrieveDogBreedById(Long id);
+    List<String> retrieveDogNames();
+}
